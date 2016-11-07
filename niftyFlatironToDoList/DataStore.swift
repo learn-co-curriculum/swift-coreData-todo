8ece0d8f62c61ed813886ae417d3d824bacfdcfe
Johann Kerr <johannkerr@gmail.com>
2016-11-07 15:29:21 -0500

    woohoo solution

---
+++ niftyFlatironToDoList/DataStore.swift
@@ -0,0 +1,47 @@
+//
+//  DataStore.swift
+//  niftyFlatironToDoList
+//
+//  Created by Johann Kerr on 11/7/16.
+//  Copyright © 2016 Johann Kerr. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class DataStore{
+    private init(){}
+    static let sharedInstance = DataStore()
+    
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "ToDo")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    
+    
+    
+    
+}
--- niftyFlatironToDoList/ViewController.swift
+++ niftyFlatironToDoList/ViewController.swift
@@ -7,19 +7,113 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import Foundation
+import CoreData
+class ViewController: UITableViewController {
+    
+    
+    var todos: [Lab] = []
+    let store = DataStore.sharedInstance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchData()
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basicCell")
+        cell?.backgroundColor = UIColor.randomColor
+        cell?.textLabel?.text = todos[indexPath.row].name!
+        return cell!
+    }
+    
+    
+    func fetchData(){
+        let managedContext = store.persistentContainer.viewContext
+        var fetchRequest = NSFetchRequest<Lab>(entityName: "Lab")
+        do{
+            self.todos = try managedContext.fetch(fetchRequest)
+        
+            
+        }catch{
+            
+        }
+       
+    }
+    
+
+    
+    
+    
+    @IBAction func addBtnPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Lab", message: "Add Topic and Name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+            
+            guard let textField = alert.textFields?.first, let nameToSave = textField.text else {
+                return
+            }
+            
+            
+            let managedContext = self.store.persistentContainer.viewContext
+            
+            var entity = NSEntityDescription.entity(forEntityName: "Lab", in: managedContext)
+            var task = NSManagedObject(entity: entity!, insertInto: managedContext) as! Lab
+            task.name = nameToSave
+            do {
+                try managedContext.save()
+                self.todos.append(task)
+            }catch{
+                
+            }
+            
+            
+            //self.labs.append(nameToSave)
+
+            
+            
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+
+}
+
+
+extension UIColor{
+    
+    class var randomColor: UIColor{
+        get {
+            let red = CGFloat(drand48())
+            let green = CGFloat(drand48())
+            let blue = CGFloat(drand48())
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+
+        }
     }
-
-
+   
 }
 
